fa7ca7732f5b6b686bba09e622ece233e3879651
Thorsten Westheider <thorsten.westheider@outlook.com>
2016-09-11 21:02:07 +0200

    Moved inline template to separate file

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,20 +7,7 @@ declare var module: {
 @Component({
   moduleId: module.id,
   selector: 'ngh-app',
-  template: `
-    <div class="ngh-viewport">
-      <md-sidenav-layout>
-        <div class="ngh-app-bar">
-          <button class="ngh-sidenav-toggle" #toggle (click)="left.open()">
-            <md-icon>menu</md-icon>
-          </button>
-          <span class="ngh-title">Angular 2 Hearthstone</span>
-        </div>
-        <md-sidenav class="ngh-sidenav-left" #left></md-sidenav>
-        <router-outlet></router-outlet>
-      </md-sidenav-layout>
-    </div>
-  `,
+  templateUrl: 'app.component.html',
   styleUrls: ['app.component.css']
 })
 export class AppComponent { 
